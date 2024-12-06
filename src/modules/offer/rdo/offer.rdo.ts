34c8a8bacb240db5dfaea99dde1b2e6c0a299e40
Dmitry Babintsev <dimababintcev@gmail.com>
2024-12-07 00:30:14 +0500

    сделал

--- src/modules/offer/rdo/offer.rdo.ts
+++ src/modules/offer/rdo/offer.rdo.ts
@@ -1,8 +1,15 @@
-import { Expose } from 'class-transformer';
+import {Expose, Type} from 'class-transformer';
 import {CityEnum} from '../../../types/city.enum.js';
 import {HousingType} from '../../../types/housing-type.enum.js';
+import {Facilities} from '../../../types/facilities.enum.js';
+import {UserType} from '../../../types/user.type.js';
+import {CoordinatesType} from '../../../types/coordinates.type.js';
+import UserRdo from '../../user/rdo/user.rdo.js';
 
 export class OfferRdo {
+  @Expose()
+  public id!: string;
+
   @Expose()
     name!: string;
 
@@ -10,10 +17,16 @@ export class OfferRdo {
     publicationDate!: Date;
 
   @Expose()
+    description!: string;
+
+  @Expose()
     city!: CityEnum;
 
   @Expose()
     previewImage!: string;
+
+  @Expose()
+    images!: string[];
 
   @Expose()
     premium!: boolean;
@@ -32,4 +45,20 @@ export class OfferRdo {
 
   @Expose()
     commentsCount!: number;
+
+  @Expose()
+    roomCount!: number;
+
+  @Expose()
+    guestCount!: number;
+
+  @Expose()
+    facilities!: Facilities[];
+
+  @Expose({name: 'userId'})
+  @Type(() => UserRdo)
+    offerAuthor!: UserType;
+
+  @Expose()
+    coordinates!: CoordinatesType;
 }

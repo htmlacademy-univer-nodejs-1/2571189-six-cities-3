3a3d7aa9c16d8b517080b9d755111a2d14e2ded9
Dmitry Babintsev <dimababintcev@gmail.com>
2024-12-07 00:43:37 +0500

    сделал

--- src/modules/offer/offer.entity.ts
+++ src/modules/offer/offer.entity.ts
@@ -1,9 +1,9 @@
-import typegoose, {defaultClasses, getModelForClass, Ref, Severity} from '@typegoose/typegoose';
-import {CityEnum} from '../../types/city.enum.js';
-import {Facilities} from '../../types/facilities.enum.js';
-import {HousingType} from '../../types/housing-type.enum.js';
-import {UserEntity} from '../user/user.entity.js';
-import {CoordinatesType} from '../../types/coordinates.type.js';
+import typegoose, { defaultClasses, getModelForClass, mongoose, Ref, Severity } from '@typegoose/typegoose';
+import { CityEnum } from '../../types/city.enum.js';
+import { CoordinatesType } from '../../types/coordinates.type.js';
+import { Facilities } from '../../types/facilities.enum.js';
+import { HousingType } from '../../types/housing-type.enum.js';
+import { UserEntity } from '../user/user.entity.js';
 
 const {prop, modelOptions} = typegoose;
 
@@ -16,18 +16,18 @@ export interface OfferEntity extends defaultClasses.Base {
   }
 })
 export class OfferEntity extends defaultClasses.TimeStamps {
-
   @prop({
-    required: true,
     type: () => String,
+    required: true,
     enum: CityEnum
   })
   public city!: CityEnum;
 
-  @prop({default: 0})
+  @prop({type: () => Number, default: 0})
   public commentsCount!: number;
 
   @prop({
+    type: () => Number,
     required: true,
     min: [100, 'Min cost is 100'],
     max: [100000, 'Max cost is 100000']
@@ -35,6 +35,7 @@ export class OfferEntity extends defaultClasses.TimeStamps {
   public cost!: number;
 
   @prop({
+    type: () => String,
     required: true,
     trim: true,
     minlength: [20, 'Min length for description is 20'],
@@ -43,32 +44,35 @@ export class OfferEntity extends defaultClasses.TimeStamps {
   public description!: string;
 
   @prop({
-    required: true,
     type: () => String,
+    required: true,
     enum: Facilities
   })
   public facilities!: Facilities[];
 
   @prop({
+    type: () => Number,
     required: true, min: [1, 'Min count of guests is 1'],
     max: [10, 'Max count of guests is 10']
   })
   public guestCount!: number;
 
   @prop({
-    required: true,
     type: () => String,
+    required: true,
     enum: HousingType
   })
   public housingType!: HousingType;
 
   @prop({
-    type: () => [String], minCount: [6, 'Images should be 6'],
+    type: () => [String],
+    minCount: [6, 'Images should be 6'],
     maxCount: [6, 'Images should be 6']
   })
   public images!: string[];
 
   @prop({
+    type: () => String,
     required: true,
     trim: true,
     minlength: [10, 'Min length for name is 10'],
@@ -82,28 +86,31 @@ export class OfferEntity extends defaultClasses.TimeStamps {
   })
   public userId!: Ref<UserEntity>;
 
-  @prop({required: true, default: false})
+  @prop({type: () => Boolean, required: true, default: false})
   public premium!: boolean;
 
-  @prop({required: true})
+  @prop({type: () => String, required: true})
   public previewImage!: string;
 
-  @prop({ required: true })
+  @prop({type: () => Date, required: true })
   public publicationDate!: Date;
 
   @prop({
+    type: () => Number,
     required: true, min: [1, 'Min rating is 1'],
     max: [5, 'Max rating is 5']
   })
   public rating!: number;
 
   @prop({
+    type: () => Number,
     required: true, min: [1, 'Min room count is 1'],
     max: [8, 'Max room count is 8']
   })
   public roomCount!: number;
 
   @prop({
+    type: () => mongoose.Schema.Types.Mixed,
     required: true,
     allowMixed: Severity.ALLOW
   })

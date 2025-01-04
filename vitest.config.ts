57a6ab5fabde3f27475fac834bec238d121736cb
Dmitry Babintsev <dimababintcev@gmail.com>
2025-01-05 00:05:44 +0500

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
-    reporters: ['default'],
-    globalSetup: './e2e/globalSetup.ts'
+    reporters: ['default']
   },
 });
